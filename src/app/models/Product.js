7207248fb53e27440e0bf57e01f27ff479f328c1
marcelogaldino <marcelinhomgo@gmail.com>
2021-03-21 15:26:05 -0300

    feat: feat: search products implemented

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -2,6 +2,13 @@ const db = require('../../config/db')
 const { date } = require('../../lib/utils')
 
 module.exports = {
+    all() {
+        return db.query(`
+            SELECT * FROM products
+            ORDER BY created_at
+        `)
+    },
+
     create(data) {
         const query = `
             INSERT INTO products (
@@ -78,5 +85,33 @@ module.exports = {
         return db.query(`
             SELECT * FROM files WHERE product_id = $1
         `, [id])
+    },
+    
+    search(params) {
+        const {filter, category} = params
+
+        let query = "",
+            filterQuery = "WHERE"
+
+        if (category) {
+            filterQuery = `${filterQuery}
+            products.category_id = ${category}
+            AND`
+        }
+
+        filterQuery = `
+        ${filterQuery}
+        products.name ilike '%${filter}%'
+        OR products.description ilike '%${filter}%'
+        `
+        
+        query = `
+        SELECT products.*,
+            categories.name AS category_name
+        FROM products
+        LEFT JOIN categories ON (categories.id = products.category_id)
+        ${filterQuery}
+        `
+        return db.query(query)
     }
 }
